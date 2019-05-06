8eba067f6c1aecbab7df46f499ab1f852474a938
pxt <pxt@176.17.3.70>
2019-05-06 09:28:26 +0800

    12

--- Demo1/src/main/java/test/Demo2.java
+++ Demo1/src/main/java/test/Demo2.java
@@ -2,9 +2,9 @@ package test;
 
 public class Demo2 {
 	public static void main(String[] args) {
+		//13
 		System.out.println("我");
 		System.out.println("1");
 		System.out.println("2");
-		//13
 	}
 }
